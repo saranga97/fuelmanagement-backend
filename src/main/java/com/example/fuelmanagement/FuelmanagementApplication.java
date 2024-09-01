43058b247162bd8d519f4254bb65eebace13b66c
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-09-01 21:19:10 +0530

    Initial commit

---
+++ src/main/java/com/example/fuelmanagement/FuelmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelmanagementApplication.class, args);
+	}
+
+}

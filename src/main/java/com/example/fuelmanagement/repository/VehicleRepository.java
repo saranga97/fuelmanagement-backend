06e6010f92e9f69d53cbeb1032a0450a441a6100
th4ruka <tharukapavith2@gmail.com>
2024-09-06 13:51:52 +0530

    #13 Add VehicleRepository interface

---
+++ src/main/java/com/example/fuelmanagement/repository/VehicleRepository.java
@@ -0,0 +1,11 @@
+package com.example.fuelmanagement.repository;
+
+import com.example.fuelmanagement.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    // Additional custom query methods can be defined here
+}
+

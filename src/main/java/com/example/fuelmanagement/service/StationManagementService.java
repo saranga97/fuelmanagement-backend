d8656d0dba09c7d2f1d474796e8bf7d41677d6e5
th4ruka <tharukapavith2@gmail.com>
2024-09-28 20:13:22 +0530

    #36 Create service to query station fuel stocks

--- src/main/java/com/example/fuelmanagement/service/StationManagementService.java
+++ src/main/java/com/example/fuelmanagement/service/StationManagementService.java
@@ -2,7 +2,9 @@ package com.example.fuelmanagement.service;
 
 import com.example.fuelmanagement.dto.StationDTO;
 import com.example.fuelmanagement.mapper.StationMapper;
+import com.example.fuelmanagement.model.FuelStock;
 import com.example.fuelmanagement.model.Station;
+import com.example.fuelmanagement.repository.FuelStockRepository;
 import com.example.fuelmanagement.repository.StationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,13 +13,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class StationRegistrationService {
+public class StationManagementService {
 
     @Autowired
     private StationRepository stationRepository;
 
     @Autowired
     private StationMapper stationMapper;
+
+    @Autowired
+    private FuelStockRepository fuelStockRepository;
 
     public StationDTO createStation(StationDTO stationDTO) {
         Station station = stationMapper.stationDTOToStation(stationDTO);
@@ -31,4 +36,9 @@ public class StationRegistrationService {
                 .map(stationMapper::stationToStationDTO)
                 .collect(Collectors.toList());
     }
+
+    public List<FuelStock> getAvailableFuelStockByStation(Long stationId) {
+        return fuelStockRepository.findByStationId(stationId);
+    }
+
 }

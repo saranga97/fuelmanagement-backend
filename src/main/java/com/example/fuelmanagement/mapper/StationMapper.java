83cacddcb14d09483e01b12a5b2de111f81af838
th4ruka <tharukapavith2@gmail.com>
2024-10-04 15:47:06 +0530

    #46 Modify StationMapper and UserMapper.

--- src/main/java/com/example/fuelmanagement/mapper/StationMapper.java
+++ src/main/java/com/example/fuelmanagement/mapper/StationMapper.java
@@ -3,12 +3,16 @@ package com.example.fuelmanagement.mapper;
 import com.example.fuelmanagement.dto.StationDTO;
 import com.example.fuelmanagement.model.Station;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface StationMapper {
     StationMapper INSTANCE = Mappers.getMapper(StationMapper.class);
 
+    @Mapping(source = "owner.username", target = "ownerUsername")
     StationDTO stationToStationDTO(Station station);
+
+    @Mapping(target = "owner", ignore = true) // Prevent direct mapping of owner
     Station stationDTOToStation(StationDTO stationDTO);
 }
--- src/main/java/com/example/fuelmanagement/mapper/UserMapper.java
+++ src/main/java/com/example/fuelmanagement/mapper/UserMapper.java
@@ -3,12 +3,16 @@ package com.example.fuelmanagement.mapper;
 import com.example.fuelmanagement.dto.UserDTO;
 import com.example.fuelmanagement.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", uses = {VehicleMapper.class})
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "password", source = "password")
     UserDTO userToUserDTO(User user);
+
+    @Mapping(target = "password", source = "password")
     User userDTOToUser(UserDTO userDTO);
 }

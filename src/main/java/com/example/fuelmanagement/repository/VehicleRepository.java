4c22d0080912d05dc0ceac5d20b2542c70a49074
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-10-20 21:27:38 +0530

    #47 VehicleRepository.java

--- src/main/java/com/example/fuelmanagement/repository/VehicleRepository.java
+++ src/main/java/com/example/fuelmanagement/repository/VehicleRepository.java
@@ -2,7 +2,8 @@ package com.example.fuelmanagement.repository;
 
 import com.example.fuelmanagement.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-    // Additional query methods if needed
 }

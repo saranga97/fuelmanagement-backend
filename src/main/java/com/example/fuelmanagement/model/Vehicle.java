f01eb3e88b30c0fb5202e0b07eaae90f1651fed1
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-10-20 21:03:54 +0530

    #43 Vehicle and Operator models updates

--- src/main/java/com/example/fuelmanagement/model/Vehicle.java
+++ src/main/java/com/example/fuelmanagement/model/Vehicle.java
@@ -1,132 +1,31 @@
 package com.example.fuelmanagement.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "vehicles")
 public class Vehicle {
-    @Getter
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Getter
     private String registrationNumber;
-    @Getter
-    private String ownerName;
-    @Getter
     private String engineNumber;
-    @Getter
-    private String vehicleClass;
-    @Getter
-    private String conditionsAndNotes;
-    @Getter
-    private String make;
-    @Getter
+    private String manufacturer;
     private String model;
-    private int yearOfManufacture;
-    private String ownershipName;
-    private boolean isMortgaged;
+    private int engineCapacity;
+    private String fuelType;
+    private int manufacturedYear;
+    private int remainingQuota;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @JoinColumn(name = "owner_id")
+    private VehicleOwner owner;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-    public void setOwnerName(String ownerName) {
-        this.ownerName = ownerName;
-    }
-
-    public void setEngineNumber(String engineNumber) {
-        this.engineNumber = engineNumber;
-    }
-
-    public void setVehicleClass(String vehicleClass) {
-        this.vehicleClass = vehicleClass;
-    }
-
-    public void setConditionsAndNotes(String conditionsAndNotes) {
-        this.conditionsAndNotes = conditionsAndNotes;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    public String getOwnerName() {
-        return ownerName;
-    }
-
-    public String getEngineNumber() {
-        return engineNumber;
-    }
-
-    public String getVehicleClass() {
-        return vehicleClass;
-    }
-
-    public String getConditionsAndNotes() {
-        return conditionsAndNotes;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public int getYearOfManufacture() {
-        return yearOfManufacture;
-    }
-
-    public void setYearOfManufacture(int yearOfManufacture) {
-        this.yearOfManufacture = yearOfManufacture;
-    }
-
-    public String getOwnershipName() {
-        return ownershipName;
-    }
-
-    public void setOwnershipName(String ownershipName) {
-        this.ownershipName = ownershipName;
-    }
-
-    public boolean isMortgaged() {
-        return isMortgaged;
-    }
-
-    public void setMortgaged(boolean mortgaged) {
-        isMortgaged = mortgaged;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
+    @ManyToOne
+    @JoinColumn(name = "vehicle_class_id")
+    private VehicleClass vehicleClass;
 }

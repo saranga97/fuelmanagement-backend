b4cba2975a2382a91ae0fc16f4b3d5feace3d6ee
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-10-22 17:31:16 +0530

    #61 Admin DTO

---
+++ src/main/java/com/example/fuelmanagement/DTO/AdminDTO.java
@@ -0,0 +1,51 @@
+package com.example.fuelmanagement.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class AdminDTO {
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

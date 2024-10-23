4372bba81fcebe756dce771a65cc95ac8ff7eba3
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-10-23 23:14:38 +0530

    #74 StationOwner

--- src/main/java/com/example/fuelmanagement/DTO/StationOwnerDTO.java
+++ src/main/java/com/example/fuelmanagement/DTO/StationOwnerDTO.java
@@ -2,7 +2,11 @@ package com.example.fuelmanagement.DTO;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class StationOwnerDTO {
 
     @NotBlank(message = "Full name is required")
@@ -21,63 +25,4 @@ public class StationOwnerDTO {
     private String idCardNumber;
 
     private String phoneNumber;
-
-    @NotBlank(message = "Station address is required")
-    private String stationAddress;
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getIdCardNumber() {
-        return idCardNumber;
-    }
-
-    public void setIdCardNumber(String idCardNumber) {
-        this.idCardNumber = idCardNumber;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getStationAddress() {
-        return stationAddress;
-    }
-
-    public void setStationAddress(String stationAddress) {
-        this.stationAddress = stationAddress;
-    }
 }
